12338330ef48759089b310d414f4a157181e3aa9
Boloyyu90 <balaputra20@gmail.com>
2025-10-31 03:31:08 +0700

    success showing redirect login page

---
+++ tailwind.config.ts
@@ -0,0 +1,8 @@
+// tailwind.config.ts
+export default {
+    content: [
+        "./src/app/**/*.{ts,tsx}",
+        "./src/components/**/*.{ts,tsx}",
+        "./src/features/**/*.{ts,tsx}",
+    ],
+};
