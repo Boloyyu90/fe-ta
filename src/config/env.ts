ca7c670c75a10897231d789d661fc9aaf5d910cf
Boloyyu90 <balaputra20@gmail.com>
2025-10-30 18:23:52 +0700

    refactor project structure to align backend layer

--- src/config/env.ts
+++
@@ -1,5 +0,0 @@
-export const ENV = {
-  API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api/v1',
-  IS_DEV: process.env.NODE_ENV === 'development',
-  IS_PROD: process.env.NODE_ENV === 'production',
-} as const;
